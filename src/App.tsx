37552460dadd89592aa38931e317d0978b8b9819
UCQ <zerda.elsword@gmail.com>
2021-01-13 01:06:07 +0900

    add: 簡単に紹介文を載せてみた

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./app.module.css";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <img
+        className={styles.githubBudge}
+        alt="GitHub commit activity"
+        src="https://img.shields.io/github/commit-activity/m/ucciqun/celebro"
+      />
+      <div className={styles.main}>
+        <img
+          src="https://pbs.twimg.com/profile_images/1344500423407718400/K-iA-fXR_400x400.jpg"
+          alt="ロゴ"
+          className={styles.logo}
+        />
+        <h1 className={styles.title}>Welcome to my pages!</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
