9746648eb87840f4d0845dfcfa6ad34af9d32160
UCQ <zerda.elsword@gmail.com>
2021-01-18 23:57:33 +0900

    fix: 組織欄作ったけど著作権的にダメかも

--- src/Organization.tsx
+++ src/Organization.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import GlassMorphism from "./GlassMorphism";
 import styles from "./Organization.module.css";
 import TraP from "./traP_logo.svg";
 
 const Organization = () => {
   return (
-    <div className={styles.organization}>
-      <img src={TraP} alt="traPロゴ" className={styles.trapLogo} />
-    </div>
+    <GlassMorphism>
+      <div className={styles.organization}>
+        <img src={TraP} alt="traPロゴ" className={styles.trapLogo} />
+        <div
+          style={{
+            borderLeft: "2px solid white",
+            height: 60,
+            margin: "0 24px",
+          }}
+        ></div>
+        <h1>絶賛募集中!</h1>
+      </div>
+    </GlassMorphism>
   );
 };
 
