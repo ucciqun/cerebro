e472a1571ea255240c90e550ebdff4f0499e4677
UCQ <zerda.elsword@gmail.com>
2021-01-18 21:58:41 +0900

    fix: とりあえずSNSはいらない

--- src/Whoami.tsx
+++ src/Whoami.tsx
@@ -6,37 +6,21 @@ import styles from "./Whoami.module.css";
 
 const Whoami = () => {
   return (
-    <>
-      <h2 className={styles.title}>WHO AM I</h2>
+    <div className={styles.whoami}>
+      <h2 className={styles.title}>
+        WHO AM <span style={{ color: "" }}>I</span>
+      </h2>
       <Divider style={{ backgroundColor: "white" }} />
 
       <p className={styles.description}>
-        東京工業大学 情報理工学院
+        私は東京工業大学 情報理工学院
         数理計算科学系でComputerScienceを学んでいる学生です。
         <br />
         web技術、デザイン、人間拡張(Human Augmentation)にも興味を持っています。
+        <br />
+        将来的にはデザイナーとプログラマーの架け橋としてtranscompileできる人間を目指しています。
       </p>
-      <div className={styles.glassMorphism}>
-        <div className={styles.snsIconContainer}>
-          <div>
-            <IconButton
-              style={{ color: "skyblue" }}
-              href="https://twitter.com/xxUCQxx"
-              target="_blank"
-            >
-              <TwitterIcon />
-            </IconButton>
-          </div>
-          <IconButton
-            href="https://github.com/ucciqun"
-            target="_blank"
-            style={{ backgroundColor: "white", color: "black" }}
-          >
-            <GitHubIcon />
-          </IconButton>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
