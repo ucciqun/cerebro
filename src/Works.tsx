4393bf38624c7b7385a063bb176de404a3ae8e23
UCQ <zerda.elsword@gmail.com>
2021-01-19 14:57:38 +0900

    add: cimicineの参考画像を追加

--- src/Works.tsx
+++ src/Works.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import GlassMorphism from "./GlassMorphism";
 
 import styles from "./Works.module.css";
+import Cimicine from "./works/Cimicine";
 
 const Works: React.FC = () => {
   return (
     <div>
       <h1 className={styles.title}>WORKS</h1>
       <Divider style={{ backgroundColor: "white" }} />
-      <GlassMorphism></GlassMorphism>
+      <Cimicine />
     </div>
   );
 };
--- src/works/Cimicine.tsx
+++ src/works/Cimicine.tsx
@@ -5,38 +5,53 @@ import CimicinePic from "../asset/cimicine.svg";
 import CimicineIcon from "../asset/cimicine_icon.svg";
 
 import Badge from "../Badge";
+import GlassMorphism from "../GlassMorphism";
 
 const Cimicine = () => {
   return (
-    <div className={styles.cimicine}>
-      <img src={CimicineIcon} alt="" className={styles.cimicineIcon} />
-      {/* <a
+    <>
+      <img
+        src={CimicinePic}
+        alt=""
+        style={{
+          position: "absolute",
+          width: 300,
+          right: 10,
+          // filter: "blur(4px)",
+        }}
+      />
+      <GlassMorphism>
+        <div className={styles.cimicine}>
+          <img src={CimicineIcon} alt="" className={styles.cimicineIcon} />
+          {/* <a
           href="https://cimicine-flow.web.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={styles.work_title}>Cimicine</h2>
         </a> */}
-      <div className={styles.cimicine_detail}>
-        <h2 className={styles.cimicine_title}>
-          Cimicine{" "}
-          <span style={{ fontSize: 12, color: "gray", opacity: 0.54 }}>
-            createdAt 2020.12
-          </span>
-        </h2>
-        <div className={styles.cimicine_badgeContainer}>
-          {["TypeScript", "React", "Figma", "AdobeXD", "Firebase"].map(
-            (d, i) => (
-              <Badge title={d} color="orange" key={i} />
-            )
-          )}
+          <div className={styles.cimicine_detail}>
+            <h2 className={styles.cimicine_title}>
+              Cimicine{" "}
+              <span style={{ fontSize: 12, color: "gray", opacity: 0.54 }}>
+                createdAt 2020.12
+              </span>
+            </h2>
+            <div className={styles.cimicine_badgeContainer}>
+              {["TypeScript", "React", "Figma", "AdobeXD", "Firebase"].map(
+                (d, i) => (
+                  <Badge title={d} color="orange" key={i} />
+                )
+              )}
+            </div>
+            <p className={styles.cimicine_description}>
+              学習ロードマップを簡単に作成し、共有できるwebアプリです。
+              デザインとフロントエンドを担当し、データベースはBaaSであるfirebaseを使用しました。
+            </p>
+          </div>
         </div>
-        <p className={styles.cimicine_description}>
-          学習ロードマップを簡単に作成し、共有できるwebアプリです。
-          デザインとフロントエンドを担当し、データベースはBaaSであるfirebaseを使用しました。
-        </p>
-      </div>
-    </div>
+      </GlassMorphism>
+    </>
   );
 };
 
