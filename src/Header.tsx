2b662ea78007a0357a807aad2a8c010766552dd9
UCQ <zerda.elsword@gmail.com>
2021-01-31 14:27:34 +0900

    add : mouseover event in header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.css";
 
 import Logo from "./asset/RYOOUCHI_logo.gif";
+import LeftLogo from "./asset/RYOOUCHI_left.svg";
+import RightLogo from "./asset/RYOOUCHI_right.svg";
 import { useHistory } from "react-router-dom";
 
 const Header = () => {
   const history = useHistory();
+  const [img, setImg] = useState(Logo);
+
   return (
     <>
       <div className={styles.header}>
-        <button className={styles.button}>WORKS</button>
-        <img src={Logo} alt="ロゴ" className={styles.logo} />
-        <button className={styles.button}>ABOUT</button>
+        <button
+          className={styles.button}
+          onClick={() => history.push("/works")}
+          onMouseOver={() => setImg(LeftLogo)}
+          onMouseOut={() => setImg(Logo)}
+        >
+          WORKS
+        </button>
+        <img
+          src={img}
+          alt="ロゴ"
+          className={styles.logo}
+          onClick={() => history.push("/")}
+        />
+        <button
+          className={styles.button}
+          onClick={() => history.push("/about")}
+          onMouseOver={() => setImg(RightLogo)}
+          onMouseOut={() => setImg(Logo)}
+        >
+          ABOUT
+        </button>
       </div>
     </>
   );
